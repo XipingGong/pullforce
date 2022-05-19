254f97db6c75246ae1f1a9f6d3ca88ba9e7fcf3c
xipinggong <xipinggong@umass.edu>
2022-05-19 14:21:51 -0400

    pull force ---------- Modified bash scripts to run simulations automatically.

--- ener.sh
+++ ener.sh
@@ -7,9 +7,32 @@
 #SBATCH -o job%j.log   # without -e, combine stdout/err
 #SBATCH -e job%j.err
 
+# @pikes, faster node
+# -------------------
+# GCC
+module load gcc/6.1.0
+export GCCTK=/cm/local/apps/gcc/6.1.0
+export CC=$GCCTK/bin/gcc
+export CXX=$GCCTK/bin/g++
+# OPENMM
+LOCAL=/home/ping/programs/openmm/build/7.5.0
+export OPENMM_DIR=$LOCAL
+export OPENMM_LIB_PATH=$LOCAL/lib
+export OPENMM_INCLUDE_PATH=$LOCAL/include/
+export OPENMM_PLUGIN_DIR=$LOCAL/lib/plugins
+# CUDA
+module load cuda/10.1.243
+export CUDATK=/cm/shared/apps/cuda/10.1.243
+export PATH=$CUDATK/lib64:$PATH
+export PATH=$CUDATK/lib64/stubs:$PATH
+export PATH=$CUDATK/bin:$PATH
+export OPENMM_CUDA_COMPILER=$CUDATK/bin/nvcc
 
-CHARMM=/home/ping/programs/charmm/build/omm_pullforce/charmm
+CHARMM='/home/ping/programs/charmm/build/omm_pullforce/charmm'
+mpirun='/home/ping/programs/openmpi/openmpi-3.0.0/build/bin/mpirun'
 
+# run simulations
+# ---------------
 $CHARMM -i ener.inp > ener.out   # charmm is an executable program
 grep ' KID  ' ener.out | awk '{print $1, $5, $6, $7}' | head -181 > forces.cpu.dat # natom = 181
 grep ' KID  ' ener.out | awk '{print $1, $5, $6, $7}' | tail -181 > forces.gpu.dat
--- nve.sh
+++ nve.sh
@@ -7,11 +7,32 @@
 #SBATCH -o job%j.log   # without -e, combine stdout/err
 #SBATCH -e job%j.err
 
+# @pikes, faster node
+# -------------------
+# GCC
+module load gcc/6.1.0
+export GCCTK=/cm/local/apps/gcc/6.1.0
+export CC=$GCCTK/bin/gcc
+export CXX=$GCCTK/bin/g++
+# OPENMM
+LOCAL=/home/ping/programs/openmm/build/7.5.0
+export OPENMM_DIR=$LOCAL
+export OPENMM_LIB_PATH=$LOCAL/lib
+export OPENMM_INCLUDE_PATH=$LOCAL/include/
+export OPENMM_PLUGIN_DIR=$LOCAL/lib/plugins
+# CUDA
+module load cuda/10.1.243
+export CUDATK=/cm/shared/apps/cuda/10.1.243
+export PATH=$CUDATK/lib64:$PATH
+export PATH=$CUDATK/lib64/stubs:$PATH
+export PATH=$CUDATK/bin:$PATH
+export OPENMM_CUDA_COMPILER=$CUDATK/bin/nvcc
 
-CHARMM=/home/ping/programs/charmm/build/omm_pullforce/charmm
-mpirun=/home/ping/programs/openmpi/openmpi-3.0.0/build/bin/mpirun
+CHARMM='/home/ping/programs/charmm/build/omm_pullforce/charmm'
+mpirun='/home/ping/programs/openmpi/openmpi-3.0.0/build/bin/mpirun'
 
 # running simulations
+# -------------------
 $CHARMM openmm=1 -i nve.inp > nve.1gpu.out
 $CHARMM openmm=0 -i nve.inp > nve.1cpu.out
 $mpirun -np 8 $CHARMM openmm=0 -i nve.inp > nve.8cpu.out # mpirun is an executable program
